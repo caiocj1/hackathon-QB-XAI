f48e5dfad0980441ba49294bd218f71da3242310
Caio Jordan <caioj2906@gmail.com>
2022-10-10 09:37:56 +0200

    updated readme.md, finished run eval

--- run_eval.py
+++ run_eval.py
@@ -1,7 +1,10 @@
 import argparse
 import torch
+import torch.nn as nn
 from models.model_unet import HackathonModel
 from dataset import HackathonDataset
+
+import pandas as pd
 
 from torch.utils.data import DataLoader
 
@@ -13,30 +16,39 @@ from collections import defaultdict
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_path', '-p')
+    parser.add_argument('--csv_path', '-p')
 
     args = parser.parse_args()
 
-    img = image.imread(args.image_path)
-    img = torch.tensor(img)
+    data = pd.read_csv('test_to_send/x-ai_test_data_without_labels.csv')
 
-    input = defaultdict()
-    input['img'] = img[None, :]
+    l = []
 
-    from models.model_unet import HackathonModel
-    model = HackathonModel.load_from_checkpoint('model_weights/unet.ckpt')
-    model.eval()
+    df = data['filename']
+    for filename in df:
+        img = image.imread(f'test_to_send/images/{filename}')
 
-    segmented = model(input)
+        img = torch.tensor(img)
+        print(len(l))
 
-    plt.imshow(segmented.cpu().detach().numpy(), cmap='Greys')
-    plt.savefig('eval_outs/seg')
+        input = defaultdict()
+        input['img'] = img[None, :]
 
-    from models.model_efficient import HackathonModel
-    model = HackathonModel.load_from_checkpoint('model_weights/efficientnet.ckpt')
-    model.eval()
+        from models.model_unet import HackathonModel
+        model = HackathonModel.load_from_checkpoint('model_weights/unet.ckpt')
+        model.eval()
 
-    has_silo = model(input)
-    f = open("eval_outs/out.txt", "w")
-    f.write(f'{has_silo}')
-    f.close()
+        segmented = model(input)
+        plt.imsave(f'test_to_send/segmented/{filename}', segmented.cpu().detach().numpy(), cmap='Greys')
+
+        from models.model_efficient import HackathonModel
+        model = HackathonModel.load_from_checkpoint('model_weights/efficientnet.ckpt')
+        model.eval()
+
+        has_silo = model(input)
+        has_silo = nn.Sigmoid()(has_silo)
+        has_silo = (has_silo > 0.5).int()
+        l.append(has_silo.cpu().detach().numpy())
+
+    data.insert(2, 'class', l, allow_duplicates=True)
+    data.to_csv('./test_to_send/answer.csv')
